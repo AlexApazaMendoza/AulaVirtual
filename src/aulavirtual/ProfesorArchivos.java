8de5c52326fc0f3c632c4dd4898f847bef965bb6
Alex <alexapazamendoza@gmail.com>
2020-02-08 22:31:48 -0500

    Se limpio algunos espacios

--- src/aulavirtual/ProfesorArchivos.java
+++ src/aulavirtual/ProfesorArchivos.java
@@ -41,7 +41,6 @@ public class ProfesorArchivos extends javax.swing.JFrame {
         initComponents();
     }
     
-    
     /*INICIA MI CODIGO*/
     private String codCursoEstudianteSemestre ="MATRI0001";
     private String codAsistencia;
@@ -413,7 +412,7 @@ public class ProfesorArchivos extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jButtonEliminarArchivo)
                     .addComponent(jButtonDescarga))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
